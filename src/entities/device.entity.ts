770e8c70cc0da798977d6c60a599075c3837c6e3
Marcus Peh <hkmpeh@gmail.com>
2022-05-11 15:37:00 +0800

    Add datetime of creation

--- src/entities/device.entity.ts
+++ src/entities/device.entity.ts
@@ -22,4 +22,7 @@ export default class Device {
   @Column({ type: 'int', default: 0 }) // Need to restrict to 0 - 255
   blue: number;
 
+  @Column({ type: 'timestamptz', default: () => 'NOW()', })
+  date_created: Date;
+
 }
