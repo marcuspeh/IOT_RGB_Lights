b1d8d9e2f34e055c33c3c58a61425e3f26bfcab4
Marcus Peh <hkmpeh@gmail.com>
2022-05-12 18:28:22 +0800

    Update database connection

--- src/db/dbConnection.ts
+++ src/db/dbConnection.ts
@@ -6,11 +6,7 @@ const parentDir = join(__dirname, '..');
 
 const connectionOpts: ConnectionOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.DB_NAME || 'rgb',
+  url: process.env.DATABASE_URL,
   entities: [
     device
   ],
