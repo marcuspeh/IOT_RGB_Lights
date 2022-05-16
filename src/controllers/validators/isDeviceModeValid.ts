83c8c29a9f911d966373593c89db6ec9d3441112
Marcus Peh <hkmpeh@gmail.com>
2022-05-16 16:26:50 +0800

    Update mode

--- src/controllers/validators/isDeviceModeValid.ts
+++ src/controllers/validators/isDeviceModeValid.ts
@@ -1,5 +1,5 @@
 function isDeviceModeValid(n: number): boolean {
-    return n >= 0 && n <= 4
+    return n >= 0 && n <= 3
 }
 
 export default isDeviceModeValid;
--- src/entities/deviceMode.ts
+++ src/entities/deviceMode.ts
@@ -1,7 +1,8 @@
 enum DeviceMode {
     OFF = 0,
     NORMAL_LIGHT = 1,
-    SOUND_REACTIVE = 2
+    RUNNING_LIGHT = 2,
+    SOUND_REACTIVE = 3
   }
 
 export default DeviceMode;
