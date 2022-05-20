bead0240c5992f2d9667040e178ed463908325fc
marcuspeh <hkmpeh@gmail.com>
2022-05-20 09:36:19 +0800

    Update codes

--- README.md
+++ README.md
@@ -8,4 +8,26 @@ This project is mainly split into 3 parts in their respective branches
 ## ESP8266
 Branch: [ESP8266](https://github.com/marcuspeh/IOT_RGB_Lights/tree/esp8266)
 
-The codes is used for nodemcu ESP8266 and uploaded using Arduino IDE.
+The codes is used for nodemcu ESP8266 and uploaded using Arduino IDE. I made use of `AsyncTimer`
+to run the querying of device info in 'pseudo' parallel which makes the entire response time 
+less laggier. If a faster response time is needed, reduce the timing from 600ms to something lower
+in this line `t.setInterval(getRequest, 600);`
+
+This codes only does basic functions without any preset. If preset lighting mode is needed, modification
+is required.
+
+## Parts needed
+- Nodemcu
+- Sound sensor
+- Neopixel RGB led strips
+
+## Connections
+Led strip 
+- Control pin: `pin 14`
+- Power: `5v`
+- Ground: `gnd`
+
+Sound sensor
+- Analogue pin: `pin 12`
+- Power: `pin 4`
+- Ground: `gnd`
--- nodemcu.ino
+++ nodemcu.ino
@@ -1,5 +1,13 @@
+/*
+ * Hi, I'm Marcus, an undergraduate at National University of Singapore, majoring 
+ * in computer science. This code is used along side with the server I created 
+ * and hosted on Heroku. If you are interest in the project, refer to 
+ * https://github.com/marcuspeh/IOT_RGB_Lights for more information.
+ */
+
 #include <Adafruit_NeoPixel.h>
 #include <Arduino_JSON.h>
+#include <AsyncTimer.h>
 #include <ESP8266WiFiMulti.h>
 #include <ESP8266HTTPClient.h>
 #include <stdlib.h>
@@ -8,14 +16,14 @@
 #define PIN 14
 #define NUM_LEDS 90
 
-//define the power for sound sensor
+//define the pins for sound sensor
 #define SOUNDPOWER 4
+#define SOUNDOUTPUT 12
 
 // define mode
 #define OFF_MODE 0
 #define NORMAL_LIGHT 1
 #define SOUND_REACTIVE 2
-#define FADE 3
 
 //create a NeoPixel strip
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
@@ -29,22 +37,28 @@ char httpGetUrl[] = "URL_LINK_GOES HERE";
 char fingerprint[] = "SSL_FINGERPRINT_GOES_HERE";
 
 // initialize rgb, brightness and mode
-unsigned char red = 0;
-unsigned char green = 0;
-unsigned char blue = 0;
-unsigned char brightness = 0;
-unsigned char lightMode = 0;
+int red = 0;
+int green = 0;
+int blue = 0;
+int brightness = 0;
+int lightMode = 0;
 
 // wifi connection
 ESP8266WiFiMulti wifiMulti;
 HTTPClient http;
 
+// Async function to HTTP GET
+AsyncTimer t;
+
 void setup()
 {
     Serial.begin(9600);
 
+    // Setup strip
     strip.begin();
     strip.show();
+
+    // Declare pin to power sound sensor
     pinMode(SOUNDPOWER, OUTPUT);
 
     // Set in station mode
@@ -53,62 +67,59 @@ void setup()
     // Register multi WiFi networks
     wifiMulti.addAP("WIFI_1", "WIFI_1_PASSWORD");
     wifiMulti.addAP("WIFI_2", "WIFI_2_PASSWORD");
-    
+
+    // Setup the query
+    t.setInterval(getRequest, 600);
 }
 
 void loop()
-{
-    // Maintain WiFi connection
-    if (wifiMulti.run() == WL_CONNECTED) {
-        getRequest();
-
-        switch (lightMode) 
-        {
-            case OFF_MODE:
-                offMode();
-                break;
-            case NORMAL_LIGHT:
-                rgbMode();
-                break;
-            case SOUND_REACTIVE:
-                soundMode();
-                break;
-            case FADE:
-                fadeMode();
-                break;
-        }
-        delay(500);
-    } else {
-      Serial.println("Connecting...");
+{ 
+    // Handle the async timer
+    t.handle();
+    
+    // Check the mode to run
+    switch (lightMode) 
+    {
+        case OFF_MODE:
+            offMode();
+            break;
+        case NORMAL_LIGHT:
+            rgbMode();
+            break;
+        case SOUND_REACTIVE:
+            soundMode();
+            break;
     }
 }
 
-// get data from server
+// connect to internet and get data from server
 static void getRequest() {
-    // Start connection
-    http.begin(httpGetUrl, fingerprint);
+    // Maintain WiFi connection
+    if (wifiMulti.run() == WL_CONNECTED) {       
+        // Start connection
+        http.begin(httpGetUrl, fingerprint);
+    
+        // Connect using get
+        int httpResponseCode = http.GET();
 
-    // Connect using get
-    int httpResponseCode = http.GET();
-
-    if (httpResponseCode > 0) {
-        String payload = http.getString();
-        JSONVar myObject = JSON.parse(payload);
-        Serial.println(myObject["data"]["device"]);  
-             
-        lightMode = int(myObject["data"]["device"]["mode"]);
-        red = int(myObject["data"]["device"]["red"]);
-        green = int(myObject["data"]["device"]["green"]);
-        blue = int(myObject["data"]["device"]["blue"]);
-        brightness = int (myObject["data"]["device"]["brightness"]);
-      }
-      else {
-        Serial.print("Error code: ");
-        Serial.println(httpResponseCode);
-      }
-      
-    // end connection
-    http.end();
+        // Process data if there is a response
+        if (httpResponseCode > 0) {        
+            // Get reponse and convert to json
+            String payload = http.getString();
+            JSONVar myObject = JSON.parse(payload); 
+                 
+            lightMode = int(myObject["data"]["device"]["mode"]);
+            red = int(myObject["data"]["device"]["red"]);
+            green = int(myObject["data"]["device"]["green"]);
+            blue = int(myObject["data"]["device"]["blue"]);
+            brightness = int (myObject["data"]["device"]["brightness"]);
+          }
+          
+        // end connection
+        http.end();
+    } else {
+      Serial.println("Connecting...");
+    }
 }
 
 //set entire strip to same color
@@ -141,27 +152,11 @@ static void soundMode() {
     digitalWrite(SOUNDPOWER, HIGH);
     strip.setBrightness(brightness);
     
-    int red = map(analogRead(12), 0, 1023, 0, 255);
+    int red = map(analogRead(SOUNDOUTPUT), 0, 1023, 0, 255);
     delay(5);
-    int green = map(analogRead(12), 0, 1023, 0, 255);
+    int green = map(analogRead(SOUNDOUTPUT), 0, 1023, 0, 255);
     delay(5);
-    int blue = map(analogRead(12), 0, 1023, 0, 255);
-    delay(5);
-    showColor(strip.Color(red, green, blue));
-    delay(100);
-}
-
-// Fade mode
-// todo
-static void fadeMode() {
-    digitalWrite(SOUNDPOWER, LOW);
-    strip.setBrightness(brightness);
-    
-    int red = map(analogRead(12), 0, 1023, 0, 255);
-    delay(5);
-    int green = map(analogRead(12), 0, 1023, 0, 255);
-    delay(5);
-    int blue = map(analogRead(12), 0, 1023, 0, 255);
+    int blue = map(analogRead(SOUNDOUTPUT), 0, 1023, 0, 255);
     delay(5);
     showColor(strip.Color(red, green, blue));
     delay(100);
